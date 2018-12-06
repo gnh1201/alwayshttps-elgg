c34ef936f41ea54ca2d5f1ecf71331d4aefa43a7
Namhyeon, Go <gnh1201@gmail.com>
2018-12-06 19:05:28 +0900

    Update start.php

--- start.php
+++ start.php
@@ -2,6 +2,6 @@
 elgg_register_event_handler('init', 'system', 'alwayshttps_init');
 
 function alwayshttps_init() {
-    elgg_register_js('alwayshttps', 'mod/alwayshttps/views/default/alwayshttps.js');
+    elgg_register_js('alwayshttps', 'mod/alwayshttps-elgg/views/default/alwayshttps.js');
     elgg_load_js('alwayshttps');
 }
